0a7db2c809c8659be88a44ba414a9c974dfcf3d7
Serge Kovbasiuk <sergekovbasiuk@Serges-MacBook-Pro.local>
2018-11-18 18:13:22 +0200

    added spread operator to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,9 @@ module.exports = {
     parserOptions: {
         ecmaVersion: 6,
         sourceType: 'module',
+        ecmaFeatures: {
+            experimentalObjectRestSpread: true,
+        },
     },
     rules: {
         'prettier/prettier': ['error', prettierOptions],
